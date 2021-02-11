1bfcc76b6f72211e64e4d36f949448a399a17aad
Jorge Reyero <jorgereyero@gmail.com>
2021-02-11 21:14:44 +0100

    fix error

--- utils/messages.js
+++ utils/messages.js
@@ -1,4 +1,4 @@
 /* eslint-disable max-len */
-const composeNewValueBetMessage = (valueBet) => `Value Ratio: ${valueBet.valueRatio.toFixed(3)}\n\nLinea: ${valueBet.line}\n\nL<a href="${valueBet.url}" target="_blank">${valueBet.match}</a>`;
+const composeNewValueBetMessage = (valueBet) => `Value Ratio: ${valueBet.valueRatio}\n\nLinea: ${valueBet.line}\n\nL<a href="${valueBet.url}" target="_blank">${valueBet.match}</a>`;
 
 module.exports = { composeNewValueBetMessage };

9149614bcbb45ad16f684a71f99e7b8555e570ab
Jorge Reyero <jorgereyero@gmail.com>
2021-02-13 20:20:33 +0100

    notify valuebets when value ration improves

--- services/valueBets.service.js
+++ services/valueBets.service.js
@@ -4,6 +4,8 @@ const Bet = require('../models/bets');
 const startBetTracking = async (sequence) => {
   const valueBet = await ValueBet.findOne({ sequence });
   if (!valueBet) return null;
+  valueBet.bet = true;
+  await valueBet.save();
   const bet = new Bet(valueBet.toJSON());
   return bet.save();
 };

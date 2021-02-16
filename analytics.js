e28c9ab4371b01bbfdaebeb1dbd2a32565d42aea
Jorge Reyero <jorgereyero@gmail.com>
2021-02-16 23:03:29 +0100

    logs for home away analysis

--- analytics.js
+++ analytics.js
@@ -66,8 +66,10 @@ const homeAway = (homeAway) => {
     localWin, localWinAvg, awayWin, awayWinAvg,
   } = homeAway;
   console.log((1 / localWin) + (1 / awayWinAvg));
-  if ((1 / localWin) + (1 / awayWinAvg)) return { valueRatio: round((1 / localWin) + (1 / awayWinAvg), 3), betTo: 'local', odds: localWin, avgOdds: localWinAvg };
+  console.log((1 / localWin) + (1 / awayWinAvg) <= valueBetLimit);
+  if ((1 / localWin) + (1 / awayWinAvg) <= valueBetLimit) return { valueRatio: round((1 / localWin) + (1 / awayWinAvg), 3), betTo: 'local', odds: localWin, avgOdds: localWinAvg };
   console.log((1 / awayWin) + (1 / localWinAvg));
+  console.log((1 / awayWin) + (1 / localWinAvg) <= valueBetLimit);
   if ((1 / awayWin) + (1 / localWinAvg) <= valueBetLimit) return { valueRatio: round((1 / awayWin) + (1 / localWinAvg), 3), betTo: 'away', odds: awayWin, avgOdds: awayWinAvg };
   return false;
 };

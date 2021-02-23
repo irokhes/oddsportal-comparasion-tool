12bf86345e77470597f68570b457e586f7fb9e3c
Jorge Reyero <jorge.reyero@timeshighereducation.com>
2021-02-23 07:05:11 +0100

    create drifted lines valiadtion (inactive for now)

--- utils/messages.js
+++ utils/messages.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 const composeNewValueBetMessage = (valueBet) => `Linea: ${valueBet.line}\n\n<a href="${valueBet.url}" target="_blank">${valueBet.match}</a>\n\nValue Ratio: ${valueBet.valueRatio}\n\n Seq: ${valueBet.sequence}`;
 const composeNewPercentageBetMessage = (valueBet) => `PERCENTAGE\n\nLinea: ${valueBet.line}\n\n<a href="${valueBet.url}" target="_blank">${valueBet.match}</a>\n\nValue Percentage: ${valueBet.percentage}\n\n`;
-const composeDriftedBet = (driftedBet) => `Diferencia entre lineas AH ${driftedBet.lineValue}  ${driftedBet.dnb ? driftedBet.dnbOdds : driftedBet.dcOdds}\n\n<a href="${driftedBet.url}" target="_blank">${driftedBet.match}</a>\n\n`;
+const composeDriftedBet = (driftedBet) => `Diferencia entre lineas AH ${driftedBet.lineValue} ${driftedBet.ahOdds} - ${driftedBet.dnbOdds ? driftedBet.dnbOdds : driftedBet.dcOdds} - ${driftedBet.linesDifference}%\n\n<a href="${driftedBet.url}" target="_blank">${driftedBet.match}</a>\n\n`;
 const composeOddsChangeBetMessage = (bet, oddsChange) => `La linea ${bet.line} ha cambiado ${oddsChange}\n\n<a href="${bet.url}" target="_blank">${bet.match}</a>\n\n`;
 module.exports = {
   composeNewValueBetMessage,

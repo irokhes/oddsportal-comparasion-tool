82c2c75ee4f3c726bcaa95a3a8f56e9cbc57099f
Jorge Reyero <jorge.reyero@timeshighereducation.com>
2021-03-01 19:34:54 +0100

    fix function name

--- utils/messages.js
+++ utils/messages.js
@@ -2,12 +2,12 @@
 const composeNewValueBetMessage = (valueBet) => `Linea: ${valueBet.line} ${valueBet.line === 'AH' || valueBet.line === 'O/U' ? valueBet.lineValue : ''} cuota: ${valueBet.odds}\n\n<a href="${valueBet.url}" target="_blank">${valueBet.match}</a>\n\nValue Ratio: ${valueBet.valueRatio}\n\n Seq: ${valueBet.sequence}`;
 const composeNewPercentageBetMessage = (valueBet) => `PERCENTAGE\n\nLinea: ${valueBet.line}\n\n<a href="${valueBet.url}" target="_blank">${valueBet.match}</a>\n\nValue Percentage: ${valueBet.percentage}\n\n`;
 const composeDriftedBet = (driftedBet) => `Diferencia entre lineas AH ${driftedBet.lineValue}  ${driftedBet.dnb ? driftedBet.dnbOdds : driftedBet.dcOdds}\n\n<a href="${driftedBet.url}" target="_blank">${driftedBet.match}</a>\n\n`;
-const composeAvgOddsChangeBetMessage = (bet, result) => (result.oddsChange
+const composeOddsChangeBetMessage = (bet, result) => (result.oddsChange
   ? `La linea ${bet.line} ha cambiado ${result.oddsChange}\n\n<a href="${bet.url}" target="_blank">${bet.match}</a>\n\n`
   : `OJO! ha subido la media ${result.oddsChange} de la linea ${bet.line} ${bet.line === 'AH' || bet.line === 'O/U' ? bet.lineValue : ''} \n\n<a href="${bet.url}" target="_blank">${bet.match}</a>\n\n`);
 module.exports = {
   composeNewValueBetMessage,
   composeNewPercentageBetMessage,
-  composeAvgOddsChangeBetMessage,
+  composeOddsChangeBetMessage,
   composeDriftedBet,
 };

b3f9bba6eabf1a304f75a1c9d378913f6a815d1f
Jorge Reyero <jorge.reyero@timeshighereducation.com>
2021-04-22 21:26:48 +0200

    reduce diff between bet365 and average

--- analytics.js
+++ analytics.js
@@ -18,20 +18,20 @@ const { frequency, valueBetLimit, percentageRuleLimit, percentageDriftedBetLimit
 
 const getDiffValue = (value) => {
   if (value > 2.5) return 0.85;
-  if (value > 2.4) return 0.87;
-  if (value > 2.3) return 0.88;
-  if (value > 2.2) return 0.88;
-  if (value > 2.1) return 0.88;
+  if (value > 2.4) return 0.88;
+  if (value > 2.3) return 0.89;
+  if (value > 2.2) return 0.89;
+  if (value > 2.1) return 0.89;
   if (value > 2) return 0.9;
-  if (value > 1.9) return 0.9;
-  if (value > 1.8) return 0.9;
-  if (value > 1.7) return 0.9;
-  if (value > 1.6) return 0.9;
-  if (value > 1.5) return 0.92;
-  if (value > 1.4) return 0.93;
-  if (value > 1.3) return 0.93;
-  if (value > 1.2) return 0.93;
-  if (value > 1.1) return 0.94;
+  if (value > 1.9) return 0.91;
+  if (value > 1.8) return 0.91;
+  if (value > 1.7) return 0.91;
+  if (value > 1.6) return 0.91;
+  if (value > 1.5) return 0.93;
+  if (value > 1.4) return 0.94;
+  if (value > 1.3) return 0.94;
+  if (value > 1.2) return 0.95;
+  if (value > 1.1) return 0.96;
   return 0.96
 }
 const twoLinesReco = ({ localWin, awayWin, awayWinAvg, localWinAvg, localUpTrend, localDownTrend, awayUpTrend, awayDownTrend }) => {

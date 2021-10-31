836d73efb36c53b73311caa881a5d2556172d48b
Jorge Reyero <jorge.reyero@timeshighereducation.com>
2021-10-31 15:21:14 +0100

    modify recobets values

--- analytics.js
+++ analytics.js
@@ -30,12 +30,12 @@ const getDiffValue = value => {
   if (value > 2) return 0.9;
   if (value > 1.9) return 0.9;
   if (value > 1.8) return 0.91;
-  if (value > 1.7) return 0.91;
-  if (value > 1.6) return 0.91;
-  if (value > 1.5) return 0.92;
+  if (value > 1.7) return 0.935;
+  if (value > 1.6) return 0.92;
+  if (value > 1.5) return 0.94;
   if (value > 1.4) return 0.93;
-  if (value > 1.3) return 0.93;
-  if (value > 1.2) return 0.93;
+  if (value > 1.3) return 0.945;
+  if (value > 1.2) return 0.94;
   if (value > 1.1) return 0.94;
   return 0.96;
 };

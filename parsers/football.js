ab4c2fb8b704bfe27ecb8f0128a97e276d780854
Jorge Reyero <jorge.reyero@timeshighereducation.com>
2021-11-05 21:40:39 +0100

    add logs for deugging

--- parsers/football.js
+++ parsers/football.js
@@ -29,6 +29,7 @@ const getFootballOdds = async (apiUrl, odds, url) => {
     }
     // ML 1ST HALF
     try {
+        console.log(cmd.replace('#MATCH#', match).replace('#PARAMS#', parameters).replace('#LINE#', MONEYLINE_FIRST_HALF));
         const moneyLineJSON = JSON.parse((await mlFirstHalfResult).replace(`globals.jsonpCallback('${baseUrl}', `, '').replace(');', ''));
         const moneyLineFirstHalfOdds = moneyLineJSON.d.oddsdata ? moneyLineJSON.d.oddsdata.back[`E-${MONEYLINE_FIRST_HALF}-0-0-0`] : null;
         odds.moneyLineFirstHalf = parse3WaysLine(moneyLineFirstHalfOdds);

e36c4110359d43e34ad3723eaf8bd2bbcc70f8e5
Jorge Reyero <jorge.reyero@timeshighereducation.com>
2021-12-05 15:41:01 +0100

    don't send notification for italian youth league

--- utils/utils.js
+++ utils/utils.js
@@ -141,8 +141,11 @@ function removePreferentialPicks(list, preferentialList) {
 function shouldBeNotified(valueBet) {
   const TERCERA_DIVISION_ESPANOLA = 'tercera-rfef-group';
   const YOUTH_LEAGUE_ALEMANIA = 'junioren-bundesliga';
+  const YOUTH_LEAGUE_ITALIA = 'primavera-1';
+
   if (valueBet.url.includes(TERCERA_DIVISION_ESPANOLA)) return false;
   if (valueBet.url.includes(YOUTH_LEAGUE_ALEMANIA)) return false;
+  if (valueBet.url.includes(YOUTH_LEAGUE_ITALIA)) return false;
 
   if (valueBet.upTrend >= 66) return false;
 

4a0f04c2f88b258dafe06375ee523eaaf679c87d
Jorge Reyero <jorge.reyero@timeshighereducation.com>
2023-02-02 11:15:49 +0100

    change pinnacle diff table

--- utils/diffValues.js
+++ utils/diffValues.js
@@ -16,7 +16,7 @@ const getDiffValue = (value) => {
   if (value > 1.1) return 0.94;
   return 0.96;
 };
-const getPinnacleDiffValue = (value) => {
+const getPinnacleDiffValueOriginal = (value) => {
   if (value > 2.5) return 0.85;
   if (value > 2.4) return 0.87;
   if (value > 2.3) return 0.88;
@@ -31,6 +31,24 @@ const getPinnacleDiffValue = (value) => {
   if (value > 1.4) return 0.93;
   if (value > 1.3) return 0.93;
   if (value > 1.2) return 0.93;
+  if (value > 1.1) return 0.94;
+  return 0.96;
+};
+const getPinnacleDiffValue = (value) => {
+  if (value > 2.5) return 0.9;
+  if (value > 2.4) return 0.9;
+  if (value > 2.3) return 0.9;
+  if (value > 2.2) return 0.9;
+  if (value > 2.1) return 0.9;
+  if (value > 2) return 0.94;
+  if (value > 1.9) return 0.94;
+  if (value > 1.8) return 0.94;
+  if (value > 1.7) return 0.94;
+  if (value > 1.6) return 0.94;
+  if (value > 1.5) return 0.94;
+  if (value > 1.4) return 0.94;
+  if (value > 1.3) return 0.94;
+  if (value > 1.2) return 0.94;
   if (value > 1.1) return 0.94;
   return 0.96;
 };

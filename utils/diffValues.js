1c3244994d11c232ca374d4a76c8807f0a3969b7
Jorge Reyero <jorge.reyero@timeshighereducation.com>
2023-02-16 09:10:48 +0100

    chage diff fucntion for pinnacle

--- utils/diffValues.js
+++ utils/diffValues.js
@@ -41,15 +41,15 @@ const getPinnacleDiffValue = (value) => {
   if (value > 2.2) return 0.9;
   if (value > 2.1) return 0.9;
   if (value > 2) return 0.93;
-  if (value > 1.9) return 0.93;
-  if (value > 1.8) return 0.93;
-  if (value > 1.7) return 0.93;
-  if (value > 1.6) return 0.93;
-  if (value > 1.5) return 0.92;
-  if (value > 1.4) return 0.92;
-  if (value > 1.3) return 0.92;
-  if (value > 1.2) return 0.92;
-  if (value > 1.1) return 0.92;
+  if (value > 1.9) return 0.94;
+  if (value > 1.8) return 0.94;
+  if (value > 1.7) return 0.94;
+  if (value > 1.6) return 0.94;
+  if (value > 1.5) return 0.94;
+  if (value > 1.4) return 0.94;
+  if (value > 1.3) return 0.94;
+  if (value > 1.2) return 0.94;
+  if (value > 1.1) return 0.94;
   return 0.93;
 };
 module.exports = {
